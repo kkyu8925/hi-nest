d9b7cce9403dc513d2764f18a0a0d557b0460ff8
kkyu8925 <kkyu8925@gmail.com>
2022-01-27 17:51:26 +0900

    add movie

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === parseInt(id));
+    if (!movie) {
+      throw new NotFoundException(`Movie with Id ${id} not found.`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: string): boolean {
+    this.movies.filter((movie) => movie.id !== parseInt(id));
+    return true;
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+}
